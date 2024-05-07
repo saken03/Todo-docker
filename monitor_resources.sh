c488cc1a2b3b1236ef104ef0939ad059e5570c0a
Saken <sakenserik07@gmail.com>
2024-05-07 13:22:09 +0500

    new orch

--- monitor_resources.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Monitoring Docker containers..."
-
-docker stats --no-stream --format "table {{.Container}}\t{{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}"
-
-echo "Docker stats monitoring complete."
